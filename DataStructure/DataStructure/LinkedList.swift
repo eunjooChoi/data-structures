b75a28e17c6a5039c8bf26c1ef393e5876f22ca0
eunjoo-choi <epcej0020@gmail.com>
2022-07-25 23:02:47 +0900

    LinkedList - push, append, insert 구현
    ## 수정 사항
    
    - 값 추가 메서드인 push, append, insert 구현.
    - insert에 필요한 node를 찾는 메서드인 node(at:) 구현

--- DataStructure/DataStructure/LinkedList.swift
+++ DataStructure/DataStructure/LinkedList.swift
@@ -14,6 +14,44 @@ struct LinkedList<T> {
     init() {}
     
     var isEmpty: Bool { return head == nil }
+    
+    mutating func push(value: T) {
+        self.head = Node(value: value, next: self.head)
+        if self.tail == nil {
+            self.tail = self.head
+        }
+    }
+    
+    mutating func append(value: T) {
+        guard self.isEmpty == false else {
+            self.push(value: value)
+            return
+        }
+        
+        self.tail?.next = Node(value: value)
+        self.tail = self.tail?.next
+    }
+    
+    mutating func insert(value: T, after node: Node<T>?) {
+        guard let node = node, self.tail !== node else {
+            self.append(value: value)
+            return
+        }
+        
+        node.next = Node(value: value, next: node.next)
+    }
+    
+    func node(at index: Int) -> Node<T>? {
+        var currentNode = self.head
+        var currentIndex = 0
+        
+        while currentNode != nil, currentIndex < index {
+            currentNode = currentNode?.next
+            currentIndex += 1
+        }
+        
+        return currentNode
+    }
 }
 
 extension LinkedList: CustomStringConvertible {
--- DataStructure/DataStructure/main.swift
+++ DataStructure/DataStructure/main.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
-let node1 = Node(value: 1)
-let node2 = Node(value: 2)
-let node3 = Node(value: 3)
+//let node1 = Node(value: 1)
+//let node2 = Node(value: 2)
+//let node3 = Node(value: 3)
+//
+//node1.next = node2
+//node2.next = node3
 
-node1.next = node2
-node2.next = node3
+//print(node1)
 
-print(node1)
+var list = LinkedList<Int>()
+list.append(value: 1)
+list.append(value: 2)
+list.append(value: 3)
+
+print(list)
+
+let middleNode = list.node(at: 1)
+list.insert(value: 10000, after: middleNode)
+
+print(list)
